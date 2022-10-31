9037d705074ba9937469ed1ba9231b9f997aa4c9
MelaineF <melaine.favennec@gmail.com>
2022-10-31 15:50:24 +0100

    add task and enum task

--- task.php
+++ task.php
@@ -2,19 +2,49 @@
 
 
 class task
- {
-      /** Declaration of attributes */
-      public type_task $task;
-      public int $doorsNumber;
+{
+    //task permet de définir la couleur par rapport au type de task selectionné
+    private type_task $task;
+    //text qui défini la tâche
+    private  string $name;
+    //level de difficulté de la tâche
+    private int $level;
+    //si true alors c'est quotidien sinon si false c'est hebdomadaire (weekly)
+    private bool $isDaily;
 
-      
+    //si c'est hebdomadaire il faut choisir le jour de la semaine qui va revenir
+    private string $day;
+    //
     
-      public function __construct(type_task $task)
-      {
-           // ↙ $this means the current context
-           $this->fuelLevel = 45.4; // <- the default value
-           //         ↖ Attribute
-           $this->doorsNumber = 3;
-           $this->wheelNumber = 4;
-      }
- }
+    public function __construct(type_task $task, string $name, int $level, bool $isDaily, string $day)
+    {
+        $this->$task = $task;
+        $this->$name = $name;
+        $this->$level = $level;
+        $this->$isDaily = $isDaily;     
+        $this->$day = $day;
+    }
+
+    public function getTask (): type_task{
+        return $task;
+    }
+
+    /*public function setTask (type_task $t){
+        $task = $t;
+    }*/
+
+    public function getName (): string{
+        return $name;
+    }
+
+    public function getLevel (): int{
+        return $level;
+    }
+
+    public function getIsDaily (): bool{
+        return $isDaily;
+    }
+    public function getDay (): string{
+        return $day;
+    }
+}
