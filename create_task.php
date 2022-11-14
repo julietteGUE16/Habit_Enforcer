ead8740692a610f024fb64981d8c542d64236723
MelaineF <melaine.favennec@gmail.com>
2022-11-14 13:59:54 +0100

    envoie de data vers la base de donnée pour task

--- create_task.php
+++ create_task.php
@@ -1,6 +1,6 @@
 <?php
 
-//todo : page create task
+//todo : vérif type écrire (upper case, lower case ,etc...)
 //vérifier si besoin /!\
 session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=bdd_tarootyn;charset=utf8;','root', ''); //on créer notre objet PDO pour pouvoir exécuter nos requetes, host --> hebergeur
@@ -20,15 +20,15 @@ $bdd = new PDO('mysql:host=localhost;dbname=bdd_tarootyn;charset=utf8;','root', 
             - isvalid
         */
 
-
+//TODO : do a function ???? createTask () ....
 if(isset($_POST['btn'])){//nom du bouton
     
     if(!empty($_POST['name']) AND !empty($_POST['type'])AND !empty($_POST['level'])){
 
-        $type = $_POST['type'];
-        $level = $_POST['level'];
-        $name = htmlspecialchars($_POST['name']);
-        echo "periode = ". $_POST['periode'] .'||';
+        $style = $_POST['type'];
+        $niveau = $_POST['level'];
+        $nom = htmlspecialchars($_POST['name']);
+        //echo "periode = ". $_POST['periode'] .'||';
         if($_POST['periode'] == "Quotidienne"){
             $jour = NULL;
              $isdaily = true;
@@ -42,27 +42,30 @@ if(isset($_POST['btn'])){//nom du bouton
             $jour = $_POST['jour'];
            
         }
-        //TODO : verif que cela marche
+       
+
         $id_users = $_SESSION['id_users'];
         $isvalid = false;
 
        if( $jour == null){
-        echo "jour est nul ||";
+        //echo "jour est nul ||";
        }
-       
 
-        echo "user = " . $id_users. " | jour = ". $jour . " | isdaily = ". $isdaily . " | isvalid = ". $isvalid . " | level = ". $level . " | name = ". $name . " | type = ". $type;
+       echo "user = " . $id_users. " | jour = ". $jour . " | isdaily = ". $isdaily . " | isvalid = ". $isvalid . " | niveau = ". $niveau . " | nom = ". $nom . " | style = ". $style. "\n";
+    
+       $insertTask = $bdd->prepare('INSERT INTO task(isvalid,nom,style,niveau,isdaily,jour,id_users)VALUES(?,?,?,?,?,?,?)');
+       echo "passage : 1\n";
+       $insertTask->execute(array($isvalid?1:0,$nom,$style,$niveau,$isdaily?1:0,$jour,$id_users));
+       echo "passage : 2\n";
+       $recupTask = $bdd->prepare('SELECT * FROM task WHERE nom = ? ');
+       $recupTask->execute(array($nom));
+       if($recupTask->rowCount() > 0){
+        $_SESSION['nom'] = $nom;
+        $_SESSION['id_task'] = $recupTask->fetch()['id_task'];
+    }
+    
+    header('Location: menu.php');
         
-        //TODO : 
-
-        /*$pseudo = htmlspecialchars($_POST['pseudo']);
-        $email = htmlspecialchars($_POST['email']);
-        $mdp = sha1($_POST['mdp']);
-        $insertUser = $bdd->prepare('INSERT INTO users(pseudo,email,mdp)VALUES(?,?,?)');
-        $insertUser->execute(array($pseudo,$email,$mdp));
-        $insertUser = $bdd->prepare('INSERT INTO task(jour,id_users,isdaily,isvalid,niveau,nom,style)VALUES(?,?,?,?,?,?,?)');
-        $insertUser->execute(array($jour, $id_users, $isdaily, $isvalid, $level, $name, $type));*/
-
     }else{
         echo "Veuillez compléter tous les champs..";
     }
