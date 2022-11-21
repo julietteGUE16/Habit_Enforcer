3a5cc37e662af4d14a9b11b882fcda9343348c4b
Lupit0t <paul.piauger@ynov.com>
2022-11-21 15:13:19 +0100

    creation page 404

---
+++ pages/page404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<html lang="en-US">
+<head>
+    <title>Connexion</title>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/cs s2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../Assets/404.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="content">
+        <h1 class="title">404 ERROR</h1>
+    </div>
+</body>
+</html>
